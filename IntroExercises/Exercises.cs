4ac3eb8a0a560869575ed5d153abed6c7092519d
Paula <pdelgado008@ikasle.ehu.eus>
2025-09-22 14:56:37 +0200

    ejer 3 hecho

--- IntroExercises/Exercises.cs
+++ IntroExercises/Exercises.cs
@@ -38,7 +38,16 @@ namespace IntroExercises
         //-1 if endIndex is less than startIndex or any of them is outside the array
         public static int Find(int[] array, int value, int startIndex, int endIndex)
         {
-            return 0;
+    
+            if (endIndex<startIndex)
+            return -1;
+             for (int i=startIndex; i <= endIndex; i++)
+            {
+                if(array[i]==value)
+                return i;
+            }
+            return -1;
+            
         }
 
         //TODO #4
